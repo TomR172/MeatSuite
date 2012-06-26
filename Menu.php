2e0eefae3ca8dd156c08b1e9bdd631ea47b0a2f4
Pat Haggerty <haggertypat@gmail.com>
2012-06-26 10:21:48 -0400

    menu improvements

--- Menu.php
+++ Menu.php
@@ -1,37 +1,7 @@
 <div class="Pages">
-
-	<ul>
-		<li> 
-			<?php if ($currentPage == "home"): ?>
-				<a class = "currentPage"
-				href="index.php?page=home">Home</a>
-			<?php else: ?>
-				<a href="index.php?page=home">Home</a>
-				<?php endif ?>
-		</li>
-		<li> 
-			<?php if ($currentPage == "about"): ?>
-				<a class = "currentPage" 
-				href="index.php?page=about">About</a>
-			<?php else: ?>
-				<a href="index.php?page=about">About</a>
-				<?php endif ?>
-		</li>
-		<li> 
-			<?php if ($currentPage == "commonCattleBreeds"): ?>
-				<a class = "currentPage" 
-				href="index.php?page=commonCattleBreeds">Common Cattle Breeds</a>
-			<?php else: ?>
-				<a href="index.php?page=commonCattleBreeds">Common Cattle Breeds</a>
-				<?php endif ?>
-		</li>
-		<li> 
-			<?php if ($currentPage == "contactInfo"): ?>
-				<a class = "currentPage" 
-				href="index.php?page=contactInfo">Contact Information</a>
-			<?php else: ?>
-				<a href="index.php?page=contactInfo">Contact Information</a>
-				<?php endif ?>
-		</li>
-	</ul>
+    <ul>
+	<?php foreach($pages as $page => $values) : ?>
+		<li><a class="<?php if($page == $currentPage) echo 'currentPage'; ?>" href="index.php?page=<?php echo $page; ?>"><?php echo $values["header"]; ?></a></li>
+	<?php endforeach; ?>
+    </ul>	
 </div>
