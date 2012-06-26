9bd4b2f458f55c18e5ad54b41ab7ae82efd7dc92
Pat Haggerty <haggertypat@gmail.com>
2012-06-26 10:04:50 -0400

    some improvements to index and Menu

--- Menu.php
+++ Menu.php
@@ -1,37 +1,36 @@
 <div class="Pages">
 
-	<?php global $page; ?>
 	<ul>
 		<li> 
-			<?php if ($page == "Home"): ?>
+			<?php if ($currentPage == "home"): ?>
 				<a class = "currentPage"
-				href="index.php?page=Home&pageFile=home">Home</a>
+				href="index.php?page=home">Home</a>
 			<?php else: ?>
-				<a href="index.php?page=Home&pageFile=home">Home</a>
+				<a href="index.php?page=home">Home</a>
 				<?php endif ?>
 		</li>
 		<li> 
-			<?php if ($page == "About"): ?>
+			<?php if ($currentPage == "about"): ?>
 				<a class = "currentPage" 
-				href="index.php?page=About&pageFile=about">About</a>
+				href="index.php?page=about">About</a>
 			<?php else: ?>
-				<a href="index.php?page=About&pageFile=about">About</a>
+				<a href="index.php?page=about">About</a>
 				<?php endif ?>
 		</li>
 		<li> 
-			<?php if ($page == "Common Cattle Breeds"): ?>
+			<?php if ($currentPage == "commonCattleBreeds"): ?>
 				<a class = "currentPage" 
-				href="index.php?page=Common Cattle Breeds&pageFile=commonCattleBreeds">Common Cattle Breeds</a>
+				href="index.php?page=commonCattleBreeds">Common Cattle Breeds</a>
 			<?php else: ?>
-				<a href="index.php?page=Common Cattle Breeds&pageFile=commonCattleBreeds">Common Cattle Breeds</a>
+				<a href="index.php?page=commonCattleBreeds">Common Cattle Breeds</a>
 				<?php endif ?>
 		</li>
 		<li> 
-			<?php if ($page == "Contact Information"): ?>
+			<?php if ($currentPage == "contactInfo"): ?>
 				<a class = "currentPage" 
-				href="index.php?page=Contact Information&pageFile=contactInfo">Contact Information</a>
+				href="index.php?page=contactInfo">Contact Information</a>
 			<?php else: ?>
-				<a href="index.php?page=Contact Information&pageFile=contactInfo">Contact Information</a>
+				<a href="index.php?page=contactInfo">Contact Information</a>
 				<?php endif ?>
 		</li>
 	</ul>
