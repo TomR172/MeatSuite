96bc30e6187edf3bc109b06d427a98dc58065b49
Thomas Risley <tjr77@cornell.edu>
2012-07-03 13:17:02 -0400

    database updated

--- db.sql
+++ db.sql
@@ -1,5 +1,24 @@
-CREATE DATABASE `db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `db`;
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 03, 2012 at 05:15 PM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `db`
+--
 
 -- --------------------------------------------------------
 
@@ -9,22 +28,24 @@ USE `db`;
 
 CREATE TABLE IF NOT EXISTS `farms` (
   `FarmName` varchar(30) DEFAULT NULL,
-   ALTER TABLE 'farms' ADD`owner` varchar(30) NOT NULL,
-   ALTER TABLE 'farms' ADD`address` varchar(30) NOT NULL,
-   ALTER TABLE 'farms' ADD`phone` varchar(30) NOT NULL,
-   ALTER TABLE 'farms' ADD`email` varchar(30) NOT NULL,
-   ALTER TABLE 'farms' ADD`website` varchar(30) NOT NULL,
-   ALTER TABLE 'farms' ADD`organic` tinyint(1) NOT NULL,
-   ALTER TABLE 'farms' ADD`beef` tinyint(1) NOT NULL,
-   ALTER TABLE 'farms' ADD`pork` tinyint(1) NOT NULL,
-   ALTER TABLE 'farms' ADD`chicken` tinyint(1) NOT NULL,
+  `owner` varchar(30) NOT NULL,
+  `address` varchar(30) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `website` varchar(30) NOT NULL,
+  `organic` tinyint(1) NOT NULL,
+  `beef` tinyint(1) NOT NULL,
+  `pork` tinyint(1) NOT NULL,
+  `chicken` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `farms`
 --
 
-/*INSERT INTO `farms` (`FarmName`, `FirstName`, `LastName`) VALUES
-('McDonalds Farm', 'Ronald', 'McDonald'),
-('Red Bard', 'Barney', 'Rubble'),
-('Red Bard', 'Barney', 'Rubble');*/
+INSERT INTO `farms` (`FarmName`, `owner`, `address`, `phone`, `email`, `website`, `organic`, `beef`, `pork`, `chicken`) VALUES
+('The Farm', 'John Doe', '123 Main Street', '607-555-1233', 'john.doe@gmail.com', 'www.joesfarm.com', 0, 0, 0, 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
