835c8a2ff41a10e6024971977be6e1dec40827e1
Pat Haggerty <haggertypat@gmail.com>
2012-07-12 14:11:09 -0400

    improved search

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
     function query($query) {
-        $con = mysql_connect("localhost", "root");
+        $con = mysql_connect("localhost", "root", "root");
 	if (!$con) {
             die ('Could not connect: ' . mysql_error());
 	}
