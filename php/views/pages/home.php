23c1dc978508f55443b6b963f2d56aa13554b978
Pat Haggerty <haggertypat@gmail.com>
2012-08-28 10:08:17 -0400

    updated homepage

--- php/views/pages/home.php
+++ php/views/pages/home.php
@@ -1,5 +1,22 @@
-<div class="container-fluid">
-    <img style ="float:left; margin: 5px 5px 5px 5px;" src="http://static.swap-bot.com/images/swaps/55393/swap.jpg"/>
-    <p>MeatSuite is an online directory of farmers that sell 1/4, 1/2, and whole animals. We provide a list of farms and information about them as well. Our goal is to help you find a farm that meets your desires.  </p>
-    <p>If you are a farmer interested in being included on the webiste, you can sign up by clicking <a class ="link" href ="index.php?page=signup"> here. </a> </p>
+<div class="hero-unit">
+    <h1>Welcome to Meat Suite!</h1>
+    <p>Pig pancetta pork fatback rump swine chicken meatloaf pork loin cow leberkas tenderloin tongue. Ham hock pastrami fatback chuck prosciutto, chicken filet mignon.</p>
+    <p><a class="btn btn-primary btn-large" href="?page=about">About &rarr;</a></p>
+</div>
+<div class="row">
+    <div class="span4">
+        <h3>Find a Farm</h3>
+        <p>Pig pancetta pork fatback rump swine chicken meatloaf pork loin cow leberkas tenderloin tongue. Ham hock pastrami fatback chuck prosciutto, chicken filet mignon.</p>
+        <p><a class="btn btn-primary" href="?page=farms">Farms &rarr;</a></p>
+    </div>
+    <div class="span4">
+        <h3>Learn More</h3>
+        <p>Pig pancetta pork fatback rump swine chicken meatloaf pork loin cow leberkas tenderloin tongue. Ham hock pastrami fatback chuck prosciutto, chicken filet mignon.</p>
+        <p><a class="btn btn-primary" href="?page=learnMore">Learn More &rarr;</a></p>
+    </div>
+    <div class="span4">
+        <h3>Add your Farm</h3>
+        <p>Pig pancetta pork fatback rump swine chicken meatloaf pork loin cow leberkas tenderloin tongue. Ham hock pastrami fatback chuck prosciutto, chicken filet mignon.</p>
+        <p><a class="btn btn-primary" href="?page=signup">Sign Up &rarr;</a></p>
+    </div>
 </div>
